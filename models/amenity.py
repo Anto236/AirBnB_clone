173b4abc15d891c57e7a2d86b45f5ae9c7bb46b6
Anto236 <Antonymbugua1237@gmail.com>
2023-02-17 16:08:01 +0000

    error correct

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Holds User class that inherits from BaseModel"""
+
+"""Subclass Amenity that inherits from BaseModel"""
+
 from models.base_model import BaseModel
 
 
 class Amenity(BaseModel):
-    """Initializes the Amenity class
-
-    Attributes:
-        name (str) - The name of the amenity
-    """
+    """Subclass Amenity that inherits from BaseModel"""
 
     name = ""
