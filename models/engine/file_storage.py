a220f324838103981470edeca88c6cdde58631f3
Anto236 <Antonymbugua1237@gmail.com>
2023-02-17 16:32:40 +0000

    error change

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Serializes instances to a JSON file and
-deserializes JSON files to instances"""
 
-import json
+"""serializes instances to a JSON file > deserialize JSON file to instances"""
+
+import models
 import os.path
+import json
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -14,39 +15,46 @@ from models.review import Review
 
 
 class FileStorage:
-    """File storage class that serializes and deserializes to and from JSON"""
+    """File storage class that serializes and deserializes to and from  JSON
+    Class Attributes:
+        file_path - path to JSON file
+        objects - dictionary used to store objects by ID
+    """
 
     __file_path = "file.json"
     __objects = {}
 
     def __init__(self):
-        """Instantiation method"""
+        """Instantation method"""
         pass
 
     def all(self):
-        """Returns the dictionary __objects"""
-        return self.__objects
+        """Returns dictionary __objects
+        """
+        return (self.__objects)
 
     def new(self, obj):
-        """Sets object into __objects with ID key pairing."""
-        key = '{}.{}'.format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """sets object into __objects with id key pairing.
+        """
+        FileStorage.__objects['{}.{}'.format(obj.__class__.__name__, obj.id)]\
+            = obj
 
     def save(self):
-        """Serializes objects to JSON file"""
+        """Serialize objects to JSON file
+        """
         ndict = {}
         for key, value in FileStorage.__objects.items():
             ndict[key] = value.to_dict()
         with open(FileStorage.__file_path, "w") as file:
-            json.dump(ndict, file)
+                json.dump(ndict, file)
 
     def reload(self):
-        """Deserializes from JSON file"""
+        """Deserialize from JSON file
+        """
         if os.path.isfile(self.__file_path):
-            with open(self.__file_path, "r") as file:
+            with open(FileStorage.__file_path, "r") as file:
                 temp = json.load(file)
                 for key, value in temp.items():
-                    class_name = value["__class__"]
-                    class_name = class_name.replace("models.", "")
-                    gclass = eval(class_name)
+                    clrel = temp[key]["__class__"]
+                    gclass = models.cdict[clrel]
                     self.__objects[key] = gclass(**value)
